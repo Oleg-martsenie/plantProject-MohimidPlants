8236f2ee1d0ee4e6fb2286f1aad4812c26280e3e
Gabriel Póvoa <povoajoao6@gmail.com>
2021-08-21 14:46:28 -0300

    Part 2

---
+++ assets/js/buynow.js
@@ -0,0 +1,84 @@
+let CactusJson = [
+    {id: 1,  
+        name:'Mandacaru',
+        img:'images/foto1.jpg', 
+        price:6.79, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 2,  
+        name:'Cacto Pedra',
+        img:'images/foto2.jpg', 
+        price:224.50, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 3,  
+        name:'Parodia',
+        img:'images/foto3.jpg', 
+        price:18.99, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 4,  
+        name:'Cacto Pera',
+        img:'images/foto4.jpg', 
+        price:8.20, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 5,  
+        name:'Cacto Pedra',
+        img:'images/foto5.jpg', 
+        price:29.99, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 6,  
+        name:'Mardi Grass',
+        img:'images/foto6.jpg', 
+        price:9.99, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in' 
+    },
+        
+    {id: 7, 
+        name:'Lily Pad',
+        img:'images/foto7.jpg', 
+        price:3.45, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 8,  
+        name:'Kiwi',
+        img:'images/foto8.jpg', 
+        price:9.99, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 9,  
+        name:'Green Platters',
+        img:'images/foto9.jpg',
+        price:0.44, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 10, name:'Golden Barrel',
+    img:'images/foto10.jpg',
+    price: 5480.7, 
+    decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 11, 
+        name:'Sunburst',
+        img:'images/foto11.jpg', 
+        price: 12.10, 
+        decription:'ryophyllum tubiflorum is very tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    },
+
+    {id: 12, 
+        name:'Luteovariegatum',
+        img:'images/foto12.jpg', 
+        price:20.99, 
+        decription:'ryophyllum tubiflorum is very drought tolerant. However, it does best with regular watering once or twice per week from the spring to fall, and less in winter.'
+    }
+];
--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,66 @@ $(document).ready(function() {
         $('.navbar').removeClass('nav-toggle')
     });
 });
+
+//Adicionando os Cactos
+
+const QS = (e) => {
+    return document.querySelector(e)
+}
+const QSA = (e) => {
+    return document.querySelectorAll(e)
+}
+
+let modalqt = 1;
+
+
+
+CactusJson.map((item, index)=> {
+    let cactoItem = QS('.models .cactu-item').cloneNode(true);
+
+    cactoItem.setAttribute('data-key', index);
+    cactoItem.querySelector('.cactu-item--img img').src = item.img
+    cactoItem.querySelector('.cactu-item--price').innerHTML = `$ ${item.price.toFixed(2)}`;
+    cactoItem.querySelector('.cactu-item--name').innerHTML = item.name;
+    cactoItem.querySelector('.cactu-item--desc').innerHTML = item.decription;
+   
+    cactoItem.querySelector('.cactu-item a').addEventListener('click', (e)=>{
+        e.preventDefault(); //Ele retira o padrão da tag 'a' de att a page
+        
+        let key = e.target.closest('.cactu-item').getAttribute('data-key');
+
+        modalqt = 1;
+
+
+        QS('.cactuBig img').src = CactusJson[key].img;
+        QS('.cactuInfo h1').innerHTML = CactusJson[key].name;
+        QS('.cactuInfo--desc').innerHTML = CactusJson[key].decription;
+        QS('.cactuInfo-actualPrice').innerHTML = `$ ${CactusJson[key].price.toFixed(2)}`;
+
+
+        QS('.cactuInfo-qt').innerHTML = modalqt;
+
+
+        QS('.cactuWindowArea').style.opacity = 0
+        QS('.cactuWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            QS('.cactuWindowArea').style.opacity = 1
+        },200);
+    });
+
+    QS('.cactu-area').append(cactoItem);
+});
+
+
+//Modal's Events
+
+function closeModal() {
+    QS('.cactuWindowArea').style.opacity = 0
+    setTimeout(() => {
+        QS('.cactuWindowArea').style.display = 'none'
+    }, 500);
+}
+
+QSA('.cactuInfo-cancelButton, .cactuInfo-cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal)
+});
